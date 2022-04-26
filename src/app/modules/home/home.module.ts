8c44c072a5f56d204fcd699822f4f2bc6a6e3c92
Nikita <sominola@gmail.com>
2022-04-26 23:16:45 +0300

    remove unused files

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,25 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { ReactiveFormsModule } from '@angular/forms';
-import {MatButtonModule} from "@angular/material/button";
 import {HomePageComponent} from "./home-page/home-page.component";
-import {HomeRoutingModule} from "./home-routing.module";
 
 @NgModule({
   declarations: [
     HomePageComponent,
   ],
   imports: [
-    CommonModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatIconModule,
-    ReactiveFormsModule,
-    HomeRoutingModule,
-    MatButtonModule,
   ],
 })
 export class HomeModule {}
